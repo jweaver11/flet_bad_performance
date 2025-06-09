c2986a147aeceac907845a27cd841d7388027b99
Joshua Weaver <joshuatweaver2000@gmail.com>
2025-06-09 14:35:38 -0500

    Added draggables. Need to fix dragging visual bug

--- src/hud/pagelets.py
+++ src/hud/pagelets.py
@@ -1,93 +1,89 @@
 '''
 Our 'pagelets' page that returns the container for
-all the active pagelets 
+all the active pagelets.
+Our pagelets are draggable, and fit pre-set sized spacers
+for more customization
 '''
 
 import flet as ft
 
 def create_pagelets(page: ft.Page):
 
-    #aspect_ratio = (page.height-100) / (page.width-500)     # aspect ratio for child containers
-
-    def drag_will_accept(e):
-        e.control.content.border = ft.border.all(
-            2, ft.Colors.BLACK45 if e.data == "true" else ft.Colors.RED
-        )
-        e.control.update()
-
-    def drag_accept(e: ft.DragTargetEvent):
-        src = page.get_control(f"{e.src_id}")
-        e.control.content.bgcolor = src.content.bgcolor
-        e.control.content.border = None
-        e.control.update()
-
-    def drag_leave(e):
-        e.control.content.border = None
-        e.control.update()
-
     d1 = ft.Draggable(
-        content_when_dragging=None,
-        content_feedback=None,
-        group="color",
+        group=1,
         content=ft.Container(
             border_radius=ft.border_radius.all(10),  # 10px radius on all corners
             expand=True,
             bgcolor=ft.Colors.GREY_900,
-            col={"xs": 12, "md": 6, "lg": 3},
             content=ft.Text("container 1")
-        )
+        ),
     )
     d2 = ft.Draggable(
-        content_when_dragging=None,
-        content_feedback=None,
-        group="color",
+        group=1,
         content=ft.Container(
             border_radius=ft.border_radius.all(10),  # 10px radius on all corners
-            expand=True,
+            height=200,
+            width=200,
             bgcolor=ft.Colors.GREY_900,
-            col={"xs": 12, "md": 6, "lg": 3},
             content=ft.Text("container 2")
+        ),
+        content_feedback=ft.Container(
+            border_radius=ft.border_radius.all(10),  # 10px radius on all corners
+            height=200,
+            width=200,
+            bgcolor=ft.Colors.GREY_900,
+            content=ft.Text("container 2")
+        ),
+    )
+   
+   # Our drag targets to format pagelets different ways
+    top_row = ft.DragTarget(
+        content=ft.Placeholder(
+            fallback_height=50,
+        )
+    )
+    left_column = ft.DragTarget(
+        content=ft.Placeholder(
+            fallback_width=50,
+        )
+    )
+    right_column = ft.DragTarget(
+        content=ft.Placeholder(
+            fallback_width=50,
+        )
+    )
+    bottom_row = ft.DragTarget(
+        content=ft.Placeholder(
+            fallback_height=50,
         )
     )
 
-    # Our stack to hold our different pagelets
-    pagelets_gv = ft.GridView(
-        expand=True,
-        runs_count=2,
-        #child_aspect_ratio=1,
-        controls=[
-            d1, 
-            d2,
-        ]
-    )
-
-
-    # Runs when page is resized - GRABS THE WHOLE PAGE, NOT PAGELETS PAGE
-    def page_resize(e):
-        pw.value = f"{page.width} px"
-        pw.update()
-
-    page.on_resized = page_resize
-
-
-    pw = ft.Text(top=20, left=20, style=ft.TextTheme.display_small)
-    page.overlay.append(pw)
 
     
     # Container for 1 or more pagelets open on main right side of screen (work area)
     pagelets_container = ft.Container(
         expand=True,
-        border_radius=ft.border_radius.all(10),
-        # alignment=ft.alignment.center,
         margin=ft.margin.only(top=0, left=0, right=6, bottom=6),
         content=ft.ResponsiveRow(
-            expand=True,
             controls=[
-                #pagelets_gv,
-                d1,
-                d2
+                ft.Column(
+                    controls=[
+                        top_row,
+                        ft.Row(
+                            expand=True,
+                            controls=[
+                                left_column,
+                                d1,
+                                d2,
+                                ft.Container(expand=True),
+                                right_column,
+                            ]
+                        ),
+                        bottom_row,
+                    ]
+                )
             ]
-        )  
+        )
     )
     
 
