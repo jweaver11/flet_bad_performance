03cc1e9f8b1d3d3e9967d2e64300a8d4a0598433
Joshua Weaver <joshuatweaver2000@gmail.com>
2025-06-25 09:17:46 -0500

    Widgets now get appended to the list independently from the list they come from. More streamlined approach

--- src/handlers/reload_widgets.py
+++ src/handlers/reload_widgets.py
@@ -1,13 +1,16 @@
 from handlers.layout_widgets import layout_widgets
 
 def reload_widgets(story):
-    story.visible_widgets.clear()    # clear our visible widget list
-    
+
     for character in story.characters:
-        print(f"{character.name}")
-        if character.visible == True:        # This line is the error
+        if character.visible and character.widget not in story.visible_widgets:
             print(f"{character.name} added to visible widgets")
-            story.visible_widgets.append(character.widget)    # Make our list current with visible widgets
+            story.visible_widgets.append(character.widget)
+        elif character.widget in story.visible_widgets and not character.visible:
+            print(f"{character.name} removed from visible widgets")
+            story.visible_widgets.remove(character.widget)
+    
+    
 
 
     layout_widgets(story.visible_widgets) # layout our widgets, and update everything
