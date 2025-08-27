feb29f5a550ecb609948555ea050fe0d94c85ae3
CBMel <147447964+CBMel@users.noreply.github.com>
2025-08-27 08:19:53 -0500

    started 'notes' widget

---
+++ src/models/notes.py
@@ -0,0 +1,33 @@
+
+
+import flet as ft
+import json
+import os
+#import data_paths
+from models.user import user
+from models.widget import Widget
+from constants.data_paths import notes_path
+
+class Notes(Widget):
+    def __init__(self, title: str):
+        #might remove title
+        self.title = title # Title of the notes
+        self.content = ""  # Content of the notes
+        self.created_at = ft.datetime.now()  # Creation timestamp
+        self.updated_at = ft.datetime.now()  # Last updated timestamp
+        
+        #data_paths.set_active_story_path(title)
+       
+        self.file_path = os.path.join(notes_path, f"{self.title}.json")
+        # ^ recommended
+        #self.file_path = data_paths.active_story_path
+        #^ import data path doesn't work and this doesn't work w/o doing that
+
+        # Metadata for the note
+        self.metadata = {
+            "title": self.title,
+            "character_count": int,
+            "created_at": None,
+            "last_modified": None
+        }
+        
