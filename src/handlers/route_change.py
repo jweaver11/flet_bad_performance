def2d91fe173f9d52ae24ec54d195cec16f4d24f
jweaver11 <joshuatweaver2000@gmail.com>
2025-12-29 16:21:33 -0600

    Simplified route change

--- src/handlers/route_change.py
+++ src/handlers/route_change.py
@@ -7,58 +7,61 @@ def route_change(e: ft.RouteChangeEvent) -> Story:
     ''' Handles changing our page view based on the new route '''
     from models.app import app
     from models.views.home import create_home_view
+    from models.views.loading import create_loading_view
 
     # Grabs our page from the event for easier reference
     page: ft.Page = e.page
 
     # Clear our views and any existing controls
     page.views.clear()
-    page.controls.clear()
 
-    # If our route is the home page, we just need to load the home view and return
-    if page.route == "/":
 
-        # Append the view manually since its just a function to return the view
-        page.views.append(create_home_view(page))
-        page.update()
-        return
-    
-    # Else if its our settings page, we load that view and return
-    elif page.route == "/settings":
+    match page.route:
+        case "/":
+            # Append the view manually since its just a function to return the view
+            page.views.append(create_home_view(page))
+            page.update()
+            return
+        case "/home":
+            # Append the view manually since its just a function to return the view
+            page.views.append(create_home_view(page))
+            page.update()
+            return
+        case "/settings":
+            app.settings.reload_settings()
+            page.views.append(app.settings)
+            page.update()
+            return
+        case "/loading":
+            page.views.append(create_loading_view(page))
+            page.update()
+            return
+        case _:
+            # Otherwise its a story route, so we need to find which one it is      
+            # new_story = None    # Set new story to none intially to handle routes that don't match any stories
 
-        app.settings.reload_settings()
-        page.views.append(app.settings)
-        page.update()
-        return
+            # Run through our stories and see which ones route matches our new route
+            for story in app.stories.values():
+                # If it matches, set our new story 
+                if story.route == page.route:
+                    new_story = story
+                    app.settings.data['active_story'] = story.route
+                    app.settings.story = story
+                    app.settings.save_dict()
 
-    # Otherwise its a story route, so we need to find which one it is
-    else:
 
-        new_story = None    # Set new story to none intially to handle routes that don't match any stories
-
-        # Run through our stories and see which ones route matches our new route
-        for story in app.stories.values():
-            # If it matches, set our new story 
-            if story.route == page.route:
-                new_story = story
-                app.settings.data['active_story'] = story.title
-                app.settings.story = story
-                app.settings.save_dict()
-                break
+                    new_story.startup()
+                    app.settings.story = new_story  # Gives our settings widget the story reference it needs
+                    page.views.append(new_story)
+                    page.update() 
+                    return
+                
             
-        
-        # If we have a story route that matches our new route, load it to the page views
-        if new_story is not None:
-            
-            
-            new_story.startup()
-
-            app.settings.story = new_story  # Gives our settings widget the story reference it needs
-            page.views.append(new_story)
-
-        # Otherwise, give us a blank page
-        else:
+            # If theres an error loading the story, go to home view
+            page.views.append(create_home_view(page))
+            page.update()
             page.open(Snack_Bar(f"Error loading story for route: {page.route}"))
                 
-        
-        page.update()
+                    
+            
+            page.update() 
